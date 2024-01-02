c30532c0bb9b8147532bd74a460f5422eb8ed02e
Stepan <121403794+StepanKoz@users.noreply.github.com>
2024-01-02 23:53:26 +0400

    Initial commit

---
+++ src/main/java/com/nawerno/promyshia/PromyshiaApplication.java
@@ -0,0 +1,13 @@
+package com.nawerno.promyshia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromyshiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromyshiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nawerno/promyshia/PromyshiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nawerno.promyshia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromyshiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
